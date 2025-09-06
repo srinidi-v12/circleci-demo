f828aac7a89f420f94d8887472413001f3e1b50c
srinidi-v12 <srinidiv12@gmail.com>
2025-09-06 12:35:49 +0530

    Add demo python project with CircleCI config

---
+++ testhello.py
@@ -0,0 +1,9 @@
+import unittest
+from hello import say_hello
+
+class TestHello(unittest.TestCase):
+    def test_greeting(self):
+        self.assertEqual(say_hello("CircleCI"), "Hello, CircleCI!")
+
+if __name__ == "__main__":
+    unittest.main()
