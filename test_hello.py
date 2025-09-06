928c5e87f0a5df113e8e2a7a2fa1f9e28cf32f81
srinidi-v12 <srinidiv12@gmail.com>
2025-09-06 12:47:43 +0530

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -3,7 +3,7 @@ from hello import say_hello
 
 class TestHello(unittest.TestCase):
     def test_greeting(self):
-        self.assertEqual(say_hello("CircleCI"), "Hello, CircleCI!")
+        self.assertEqual(say_hello("CircleCI"), "Hey, CircleCI!")
 
 if __name__ == "__main__":
     unittest.main()
